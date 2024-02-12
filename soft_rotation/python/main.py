10132f4e843a25d1e2c46ebf9f5da891f352bfb7
Ting Chun Liu <8765498@gmail.com>
2024-02-12 23:44:34 +0100

    soft rotation!

---
+++ soft_rotation/python/main.py
@@ -0,0 +1,100 @@
+import threading
+import math
+from pythonosc.dispatcher import Dispatcher
+from pythonosc import osc_server
+from pythonosc import udp_client
+
+import argparse
+import time
+from sendingOSC import sendOSCtoVisual_question,sendOSCtoVisual_answer
+from sendingOSC import sendOSCtoMax_answer,sendOSCtoMax_question
+
+from textGenerator import TextGenerator
+from speechRecognition import AudioRecorder
+
+generator_agent = TextGenerator('''
+                                you are a phylosophical learner machine, answer only with one short sentence.  
+                                you are allow to lie, and not reveal that you are lying.
+                                you are expert to  phylosophy in love.
+                                be elabrorate, but no so difficult langauge.
+                                '''
+                                ,role = 'agent')    
+generator_tourist = TextGenerator('''
+                                you are a poet, a really good one, answer only with one sentence,
+                                you know every love song lyric, but cheesy and really famous ones.
+                                be creative, ask random questions, like 'what is the color of love' or so on.
+                                  
+                                directly answer to the answers, don't comments on it.
+                                  
+
+                                
+                                ''',role = 'client'
+                                )
+
+loop_running = False
+
+def conversation():
+    global loop_running
+    global generator_agent
+    global generator_tourist
+    #input_text = transrcibe  # Replace with your actual prompt
+    input_text = "Hi"
+    while loop_running:
+        input_text,filepath,duration_seconds = generator_agent.speechGPT(input_text, 0)
+        sendOSCtoMax_answer(0,input_text+"(answer in one, or two short sentence)",filepath)
+        time.sleep(duration_seconds)
+        #sendOSCtoVisual_question(input_text)
+        input_text,filepath,duration_seconds = generator_tourist.speechGPT(input_text, 2)
+        sendOSCtoMax_question(1,input_text+"(answer in one, or two short sentence)",filepath)
+        time.sleep(duration_seconds)
+
+        #sendOSCtoVisual_answer(input_text)
+
+
+def start_conversation():
+    global loop_running
+    if not loop_running:
+        print("start conversation")
+        loop_running = True
+        loop_thread = threading.Thread(target=conversation)
+        loop_thread.start()
+
+def stop_conversation():
+    global loop_running
+    print("stop conversation")
+    loop_running = False
+
+if __name__ == "__main__":
+
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip",
+    default="127.0.0.1", help="The ip to listen on")
+    parser.add_argument("--port",
+    type=int, default=5005, help="The port to listen on")
+    args = parser.parse_args()
+
+    recorder = AudioRecorder()
+    #recording_id,channel = recorder.list_input_devices()
+    #output_id,channel = recorder.list_output_devices()
+    #print(id,channel)
+    #transrcibe = record.record_and_transcribe(_id = id,_channels = channel)
+
+
+    dispatcher = Dispatcher()
+    dispatcher.map("/start_recording", lambda unused_addr: recorder.start_recording())
+    dispatcher.map("/stop_recording", lambda unused_addr: recorder.stop_recording())
+    dispatcher.map("/start_conversation", lambda unused_addr: start_conversation())
+    dispatcher.map("/stop_conversation", lambda unused_addr: stop_conversation())
+
+
+    #dispatcher.map("/voice", voice_handler, "voice")
+
+    server = osc_server.ThreadingOSCUDPServer(
+    (args.ip, args.port), dispatcher)
+    print("Serving on {}".format(server.server_address))
+
+    #conversation(5)
+
+
+    server.serve_forever()
---
+++ soft_rotation/python/sendingOSC.py
@@ -0,0 +1,60 @@
+
+
+import argparse
+
+
+from pythonosc.dispatcher import Dispatcher
+from pythonosc import osc_server
+from pythonosc import udp_client
+
+def sendOSCtoVisual_question(output):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", default="127.0.0.1",
+    help="The ip of the OSC server")
+    parser.add_argument("--port", type=int, default=12001,
+    help="The port the OSC server is listening on")
+    args = parser.parse_args()
+
+    client = udp_client.SimpleUDPClient(args.ip, args.port)
+    client.send_message("/question", output)
+
+
+def sendOSCtoVisual_answer(output):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", default="127.0.0.1",
+    help="The ip of the OSC server")
+    parser.add_argument("--port", type=int, default=12001,
+    help="The port the OSC server is listening on")
+    args = parser.parse_args()
+
+    client = udp_client.SimpleUDPClient(args.ip, args.port)
+    client.send_message("/answer", output)
+
+
+
+def sendOSCtoMax_answer(person,text,fileName):
+    print("sending to Answer")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", default="127.0.0.1",
+    help="The ip of the OSC server")
+    parser.add_argument("--port", type=int, default=12005,
+    help="The port the OSC server is listening on")
+    args = parser.parse_args()
+
+    client = udp_client.SimpleUDPClient(args.ip, args.port)
+    
+    client.send_message("/fileNameAnswer", "/Users/AprilCoffee/Documents/GitHub/I-am-sitting-in-latent-space/soft_rotation/python/" + fileName)
+
+
+def sendOSCtoMax_question(person,text,fileName):
+    print("sending to Question")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", default="127.0.0.1",
+    help="The ip of the OSC server")
+    parser.add_argument("--port", type=int, default=12005,
+    help="The port the OSC server is listening on")
+    args = parser.parse_args()
+
+    client = udp_client.SimpleUDPClient(args.ip, args.port)
+    client.send_message("/fileNameQuestion", "/Users/AprilCoffee/Documents/GitHub/I-am-sitting-in-latent-space/soft_rotation/python/" + fileName)
